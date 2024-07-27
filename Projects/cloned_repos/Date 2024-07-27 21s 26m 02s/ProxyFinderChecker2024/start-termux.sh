46f122e9183fb829486709ee7c1ea640e592a8b2
amirroox <amirroox@yahoo.com>
2024-07-27 21:34:54 +0000

    Update and clone latest repositories at 2024-07-27 21:34:54.145465709

---
+++ Projects/cloned_repos/Date 2024-07-27 21s 26m 02s/ProxyFinderChecker2024/start-termux.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -eu
+
+python3 -m venv --upgrade-deps .venv
+.venv/bin/python3 -m pip install -U --disable-pip-version-check --editable .[termux]
+.venv/bin/python3 -m proxy_scraper_checker
